b4f156ca3b0ab9214c3aae649eab9af8f801b7be
ru <1500480941@qq.com>
2019-03-19 12:47:40 +0800

    3

---
+++ src/ccc/rrr.java
@@ -0,0 +1,12 @@
+package ccc;
+/**
+ * <p>Title: rrr.java</p>
+ * <p>Description: xxxx平台软件</p>
+ * <p>Copyright: Copyright (c) 2011-2012 xxx信息技术有限公司</p>
+ * <p>Company: XXX信息技术有限公司</p>
+ * @author 作者名
+ * @version 1.0 创建时间：2019年3月19日 下午12:47:16
+ */
+public class rrr {
+
+}
